b36700eb8ae558301c90f6330146efb9d6b55509
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2025-08-11 01:53:18 +0100

    feat: Enhance Navbar and Sidebar with motion animations
    - Updated Navbar to use motion.header for animated transitions
    - Updated Sidebar to use motion.aside for animated transitions

--- src/app/(dash)/_layout/navbar.tsx
+++ src/app/(dash)/_layout/navbar.tsx
@@ -1,8 +1,13 @@
 "use client";
-import { header } from "framer-motion/client";
+import { motion } from "framer-motion";
 
 export function Navbar() {
   return (
-    <header className="flex items-center justify-between default-spacing"></header>
+    <motion.header
+      animate={{ y: 0 }}
+      exit={{ y: -100 }}
+      initial={{ y: -100 }}
+      className="flex items-center justify-between default-spacing"
+    ></motion.header>
   );
 }
--- src/app/(dash)/_layout/sidebar.tsx
+++ src/app/(dash)/_layout/sidebar.tsx
@@ -1,3 +1,13 @@
+"use client";
+import { motion } from "framer-motion";
+
 export function Sidebar() {
-  return <aside className="flex flex-col basis-80 bg-white shadow"></aside>;
+  return (
+    <motion.aside
+      animate={{ x: 0 }}
+      exit={{ x: -100 }}
+      initial={{ x: -100 }}
+      className="flex flex-col basis-80 bg-white shadow"
+    ></motion.aside>
+  );
 }
