b209e01f687555437e4f48249c3a20ad57c73c3f
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2025-08-11 01:43:48 +0100

    feat: Implemented root layout and main page structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import "styles/global.css";
+import * as fonts from "app/fonts";
+import type { Metadata } from "next";
+import { ToastContainer } from "react-toastify";
+import AuthProvider from "components/providers/auth.provider";
+import QueryProvider from "components/providers/query.provider";
+
+export const metadata: Metadata = {
+  title: "Kantin Kwari",
+  icons: "/favicon.svg",
+};
+
+export default function RootLayout(props: React.PropsWithChildren) {
+  const fontsClassName = Object.values(fonts).map((item) => item.variable);
+
+  return (
+    <html lang="en" className={fontsClassName.join(" ")}>
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        />
+      </head>
+
+      <body>
+        <AuthProvider>
+          <QueryProvider>
+            <ToastContainer />
+            <div id="portal" />
+            <div id="root">{props.children}</div>
+          </QueryProvider>
+        </AuthProvider>
+      </body>
+    </html>
+  );
+}
